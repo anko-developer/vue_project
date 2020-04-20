4fe72c26af79351ab40cc404cf7bf091e7efecf3
anko-developer <59523270+anko-developer@users.noreply.github.com>
2020-04-20 16:52:26 +0900

    Vue animation

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="todo__container">
-        <ul class="todo__list">
+        <!-- <ul class="todo__list"> -->
+        <transition-group class="todo__list" name="list" tag="ul">
             <li class="todo__item shadow" v-for="(todoItem, index) in propsdata" :key="todoItem">
                 <i class="check-icon fas fa-check" aria-hidden="true"></i>
                 {{ todoItem }}
@@ -8,7 +9,8 @@
                     <i class="far fa-trash-alt" aria-hidden="true"></i>
                 </span>
             </li>
-        </ul>
+        </transition-group>
+        <!-- </ul> -->
     </section>
 </template> 
 
@@ -25,7 +27,12 @@ export default {
 
 <style lang="scss">
     .todo__list {margin-top:0;padding-left:0;list-style-type:none}
-    .todo__item {display:flex;min-height:50px;height:50px;margin:0.5rem 0;padding:0 0.9rem;border-radius:5px;line-height:50px;background:#fff}
+    .todo__item {display:flex;overflow:hidden;min-height:50px;height:50px;margin:0.5rem 0;padding:0 0.9rem;border-radius:5px;text-overflow:ellipsis;white-space:nowrap;line-height:50px;background:#fff}
     .check-icon {margin-right:8px;line-height:45px;color:#62acde}
     .remove-btn {margin-left:auto;color:#de4343}
+
+    .list-enter-active,
+    .list-leave-active {transition:all 1s}
+    .list-enter,
+    .list-leave-to {opacity:0;transform:translateY(30px)}
 </style>
